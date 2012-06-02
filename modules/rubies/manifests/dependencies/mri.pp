3f837322b6902bc3e4221ec23edda85b8f49cae1
Robert Schulze <robert@dotless.de>
2012-06-02 02:54:52 +0200

    removing default packages, which will get installed via a module

--- modules/rubies/manifests/dependencies/mri.pp
+++ modules/rubies/manifests/dependencies/mri.pp
@@ -1,7 +1,7 @@
 
 class rubies::dependencies::mri {
 	
-	package{ ['build-essential', 'openssl', 'libreadline6', 'libreadline6-dev', 'curl', 'git-core', 'zlib1g', 'zlib1g-dev', 'libssl-dev', 'libyaml-dev', 'libsqlite3-0', 'libsqlite3-dev', 'sqlite3', 'libxml2-dev', 'libxslt-dev', 'autoconf', 'libc6-dev', 'ncurses-dev', 'automake', 'libtool', 'bison', 'subversion']:
+	package{ ['build-essential', 'openssl', 'libreadline6', 'libreadline6-dev', 'curl', 'zlib1g', 'zlib1g-dev', 'libssl-dev', 'libyaml-dev', 'libsqlite3-0', 'libsqlite3-dev', 'sqlite3', 'libxml2-dev', 'libxslt-dev', 'autoconf', 'libc6-dev', 'ncurses-dev', 'automake', 'libtool', 'bison', 'subversion']:
 	  ensure => present
   }
 	
